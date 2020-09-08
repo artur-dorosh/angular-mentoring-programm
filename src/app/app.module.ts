3eee0b54008154e29f5ac987bafeba5c683ed152
Artemii Dorosh <doroshartemii@gmail.com>
2020-09-08 18:28:09 +0300

    Implemented markup and some simple logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CoursesPageComponent } from './components/courses-page/courses-page.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchComponent } from './components/courses-page/search/search.component';
+import { CoursesListComponent } from './components/courses-page/courses-list/courses-list.component';
+import { CourseItemComponent } from './components/courses-page/course-item/course-item.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CoursesPageComponent,
+    BreadcrumbsComponent,
+    LogoComponent,
+    SearchComponent,
+    CoursesListComponent,
+    CourseItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
