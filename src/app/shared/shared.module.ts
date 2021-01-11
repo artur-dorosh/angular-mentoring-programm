28f543c0a098fc73e602426856803c78da37a54e
Artemii Dorosh <doroshartemii@gmail.com>
2021-01-11 17:43:40 +0200

    Add routing

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ErrorComponent
+  }
+];
+
+@NgModule({
+  declarations: [ErrorComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class SharedModule { }
